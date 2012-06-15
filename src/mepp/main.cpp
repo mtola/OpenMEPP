ce8e96930923ca33b16392a346d6fdc604a9f703
Martial Tola <martial.tola@liris.cnrs.fr>
2012-06-15 16:13:45 +0200

    add OpenMeshToCGALConverter

--- src/mepp/main.cpp
+++ src/mepp/main.cpp
@@ -26,9 +26,12 @@
 
 #include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
 
-
 #include <ImporterCGAL.h>
 #include <ExporterCGAL.h>
+
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/Polyhedron_incremental_builder_3.h>
 
 
 #include <QApplication>
@@ -54,7 +57,8 @@ struct MyTraits : public OpenMesh::DefaultTraits
 {
 	HalfedgeAttributes(OpenMesh::Attributes::PrevHalfedge);
 };
-typedef OpenMesh::TriMesh_ArrayKernelT<MyTraits>	MyMesh;
+typedef OpenMesh::PolyMesh_ArrayKernelT<MyTraits>	MyMesh1;
+//typedef OpenMesh::TriMesh_ArrayKernelT<MyTraits>	MyMesh1;
 
 // Define my personal traits
 struct MyTraits2 : OpenMesh::DefaultTraits
@@ -75,13 +79,13 @@ struct MyTraits2 : OpenMesh::DefaultTraits
 };
 // Define my mesh with the new traits!
 typedef OpenMesh::PolyMesh_ArrayKernelT<MyTraits2>	MyMesh2;
+//typedef OpenMesh::TriMesh_ArrayKernelT<MyTraits2>	MyMesh2;
 
 
 template <typename M>
 bool 
-open_mesh( const char* _filename, OpenMesh::IO::Options _opt )
+open_mesh( M& mesh_, const char* _filename, OpenMesh::IO::Options _opt )
 {
-	M mesh_;
 	OpenMesh::IO::Options opt_; // mesh file options
 
   // load mesh
@@ -181,6 +185,69 @@ void loadStyleSheet()
     }
 }
 
+// A modifier creating a polyhedron with the incremental builder.
+template <class HDS, class Mesh>
+class Build_polyhedron : public CGAL::Modifier_base<HDS>
+{
+public:
+    Build_polyhedron(Mesh& _mesh) : mesh_(_mesh) {}
+
+    void operator()(HDS& hds)
+	{
+		// Postcondition: 'hds' is a valid polyhedral surface.
+        CGAL::Polyhedron_incremental_builder_3<HDS> B(hds, true);
+
+        B.begin_surface(mesh_.n_vertices(), mesh_.n_faces(), mesh_.n_halfedges());
+
+        typedef typename HDS::Vertex   Vertex;
+        typedef typename Vertex::Point Point;
+
+		// vertex
+		typename Mesh::VertexIter vit;
+		for(vit=mesh_.vertices_begin(); vit!=mesh_.vertices_end(); ++vit)
+		{
+			B.add_vertex(Point(mesh_.point(vit)[0], mesh_.point(vit)[1], mesh_.point(vit)[2]));
+		}
+
+		// facet
+		typename Mesh::ConstFaceIter		fIt(mesh_.faces_begin()), fEnd(mesh_.faces_end());
+		typename Mesh::ConstFaceVertexIter	fvIt;
+		for (; fIt!=fEnd; ++fIt)
+		{
+			B.begin_facet();
+
+			// only for a triangle !!!
+			/*fvIt = mesh_.cfv_iter(fIt.handle()); 
+			B.add_vertex_to_facet(fvIt.handle().idx());
+			++fvIt;
+			B.add_vertex_to_facet(fvIt.handle().idx());
+			++fvIt;
+			B.add_vertex_to_facet(fvIt.handle().idx());*/
+
+			for (fvIt=mesh_.cfv_iter(fIt.handle()); fvIt; ++fvIt)
+				B.add_vertex_to_facet(fvIt.handle().idx());
+
+			B.end_facet();
+		}
+
+        B.end_surface();
+
+		if (B.check_unconnected_vertices())
+			B.remove_unconnected_vertices();
+    }
+
+private:
+  Mesh& mesh_;
+};
+
+typedef CGAL::Simple_cartesian<float>		Kernel1;
+typedef CGAL::Polyhedron_3<Kernel1>         Polyhedron1;
+typedef Polyhedron1::HalfedgeDS             HalfedgeDS1;
+
+typedef CGAL::Simple_cartesian<double>		Kernel2;
+typedef CGAL::Polyhedron_3<Kernel2>         Polyhedron2;
+typedef Polyhedron2::HalfedgeDS             HalfedgeDS2;
+
 /*!
  * \fn int main(int argc, char *argv[])
  * \brief Main function.
@@ -200,12 +267,19 @@ int main(int argc, char *argv[])
 	window.show();
 
 	OpenMesh::IO::Options opt;
-	open_mesh<MyMesh>("C:\\Users\\noname\\Desktop\\face.texture.ply\\face.ply", opt);
-	open_mesh<MyMesh2>("C:\\Users\\noname\\Desktop\\face.obj\\face.obj", opt);
 
-	open_texture<MyMesh>("C:\\Users\\noname\\Desktop\\face.texture.ply\\face_skin_hi.bmp");
+	MyMesh1 mesh1;	
+	open_mesh<MyMesh1>(mesh1, "C:\\Users\\noname\\Desktop\\face.texture.ply\\face.ply", opt);
+	std::cout << "--> (vertices: " << mesh1.n_vertices() << " - faces: " << mesh1.n_faces() << " - edges: " << mesh1.n_edges() << ")\n\n";
+
+	MyMesh2 mesh2;
+	open_mesh<MyMesh2>(mesh2, "C:\\Users\\noname\\Desktop\\face.obj\\face.obj", opt);
+	std::cout << "--> (vertices: " << mesh2.n_vertices() << " - faces: " << mesh2.n_faces() << " - edges: " << mesh2.n_edges() << ")\n\n";
+
+	open_texture<MyMesh1>("C:\\Users\\noname\\Desktop\\face.texture.ply\\face_skin_hi.bmp");
 	open_texture<MyMesh2>("C:\\Users\\noname\\Desktop\\face.obj\\face_skin_hi.jpg");
 
+#if(0)
 	OpenMesh::IO::ImporterCGAL/*<Mesh>*/ importer/*(_mesh)*/;
 	std::cout << "--> Loading with ImporterCGAL\n";
 	if ( OpenMesh::IO::IOManager().read("C:\\Users\\noname\\Desktop\\face.obj\\face.obj", importer, opt) )
@@ -219,6 +293,19 @@ int main(int argc, char *argv[])
 		std::cout << "--> Writing done (vertices: " << exporter.n_vertices() << " - faces: " << exporter.n_faces() << ")\n\n";
 	else
 		std::cout << "--> Writing NOT done!\n\n";
+#endif
+
+	std::cout << "--> OpenMeshToCGALConverter\n";
+	Polyhedron1 P1;
+    Build_polyhedron<HalfedgeDS1, MyMesh1> polyhedron1(mesh1);
+    P1.delegate(polyhedron1);
+	std::cout << "--> (vertices: " << P1.size_of_vertices() << " - faces: " << P1.size_of_facets() << " - edges: " << (P1.size_of_halfedges() >> 1) << ")\n\n";
+
+	std::cout << "--> OpenMeshToCGALConverter\n";
+	Polyhedron2 P2;
+    Build_polyhedron<HalfedgeDS2, MyMesh2> polyhedron2(mesh2);
+    P2.delegate(polyhedron2);
+	std::cout << "--> (vertices: " << P2.size_of_vertices() << " - faces: " << P2.size_of_facets() << " - edges: " << (P2.size_of_halfedges() >> 1) << ")\n\n";
 
     return app.exec();
 }
